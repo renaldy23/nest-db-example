0b8d712165f6abc33a96aec808c365a0a0e79092
mrenaldyr <mrenaldyr03@gmail.com>
2023-09-27 11:04:50 +0700

    feat: add database module and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config/dist';
+import { DatabaseModule } from './database/database.module';
+import { TodoModule } from './todo/todo.module';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().allow(''),
+        DB_DATABASE: Joi.string().required(),
+        DB_SYNCRONIZE: Joi.boolean().required(),
+      }),
+    }),
+    DatabaseModule,
+    TodoModule,
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/database/database.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+        synchronize: configService.get('DB_SYNCRONIZE'),
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
